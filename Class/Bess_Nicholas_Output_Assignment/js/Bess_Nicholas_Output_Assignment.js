6a8944282d26638123e34de9a880019f875d6347
NBESS <nicholasdbess@gmail.com>
2015-07-10 00:41:54 -0400

    Final Commit
    Adds var finalMessage, displays it and adds a period to the end of
    several lines.

--- Class/Bess_Nicholas_Output_Assignment/js/Bess_Nicholas_Output_Assignment.js
+++ Class/Bess_Nicholas_Output_Assignment/js/Bess_Nicholas_Output_Assignment.js
@@ -3,7 +3,7 @@
  */
 //Nicholas Bess, Term: C201507, Assignment: Output Assignment
 
-var myBirthplace = "San Diego, CA"; //Assigns string variable
+var myBirthplace = "San Diego, CA."; //Assigns string variable
 var myGreeting = "Hi, my name is Nicholas Bess. I was born in";//Assigns string variable
 var isMale = true;//Assigns Boolean variable
 var birthMonth = "February";//Assigns string variable
@@ -11,11 +11,12 @@ var birthDay = 22; //Assigns number variable
 var birthYear = 1983; //Assigns number variable
 var birthdayGreeting = "I was born on";//Assigns string variable
 var farewellGreeting = "That's a \"Tad-bit\" of information about me.";//Assigns string variable containing escape characters
-var thankYouMessage = "Thanks for your time";//Assigns string variable
+var thankYouMessage = "Thanks for your time.";//Assigns string variable
+var finalMessage = "Have a wonderful day!";//Assigns string variable
 
 var greetingMessage = myGreeting + " " + myBirthplace;//Assigns concatenated string
-var birthdayMessage = birthdayGreeting + " " + birthDay + " " + birthMonth + " " + birthYear;//Assigns concatenated string
-var farewellMessage = farewellGreeting + " " + thankYouMessage;//Assigns concatenated string
+var birthdayMessage = birthdayGreeting + " " + birthDay + " " + birthMonth + " " + birthYear + ".";//Assigns concatenated string
+var farewellMessage = farewellGreeting + " " + thankYouMessage + "\n" + finalMessage;//Assigns concatenated string
 
 
 console.log(greetingMessage);//Tells console to display variable declared by greetingMessage
