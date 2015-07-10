ddee035da642505129ae83df6c3efda0e91b9fb3
NBESS <nicholasdbess@gmail.com>
2015-07-09 23:42:25 -0400

    Adds greeting and birthday messages

--- Class/Bess_Nicholas_Output_Assignment/js/Bess_Nicholas_Output_Assignment.js
+++ Class/Bess_Nicholas_Output_Assignment/js/Bess_Nicholas_Output_Assignment.js
@@ -3,4 +3,16 @@
  */
 //Nicholas Bess, Term: C201507, Assignment: Output Assignment
 
-var myBirthplace = "San Diego, CA"
+var myBirthplace = "San Diego, CA";
+var myGreeting = "Hi, my name is Nicholas Bess. I was born in";
+var isMale = true;
+var birthMonth = "February";
+var birthDay = 22;
+var birthYear = 1983;
+var birthdayGreeting = "I was born on";
+
+var greetingMessage = myGreeting + " " + myBirthplace;
+var birthdayMessage = birthDay + " " + birthMonth + " " + birthYear;
+
+console.log(greetingMessage);
+console.log(birthdayMessage);
