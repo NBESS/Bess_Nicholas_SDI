ae8516b1b89940d3a4f441b066465e2a47ac6dbb
NBESS <nicholasdbess@gmail.com>
2015-07-13 11:34:32 -0400

    Completes Slice of Pie part 1

--- Class/ActivityFiles/Expression Worksheet/js/script.js
+++ Class/ActivityFiles/Expression Worksheet/js/script.js
@@ -12,10 +12,20 @@ console.log("Sparky is " + sparkysHumanYears + " human years old which is " + sp
 
 //Calculate the amount of pizza each partygoer will receive
 
-var pizzaAmount = 2;
-var pizzaEaters = 8;
+var pizzaAmount = 3;
+var pizzaEaters = 5;
 
 //multiply amount of pizza by 8
 
 var pizzaSlices = pizzaAmount * 8;
-console.log(pizzaSlices);
+
+//divide slices evenly between partygoers
+
+var slicesServed = pizzaSlices / pizzaEaters;
+
+//print out number of slices each person ate
+
+console.log("Each person ate " + slicesServed + " slices of pizza at the party.");
+
+
+
