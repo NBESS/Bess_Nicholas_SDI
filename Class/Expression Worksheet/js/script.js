a615a441a79e5867ae411482e89f9da55202d9f6
NBESS <nicholasdbess@gmail.com>
2015-07-13 09:51:53 -0400

    Initial commit for Expression Worksheet activity

---
+++ Class/Expression Worksheet/js/script.js
@@ -0,0 +1,12 @@
+//Convert dog years to human years
+
+var sparkysHumanYears = 3;
+
+//Dog years are 7 more than human years
+
+var sparkysDogYears = sparkysHumanYears * 7;
+
+//print out Sparky's age in dog years
+
+console.log("Sparky is " + sparkysHumanYears + " human years old which is " + sparkysDogYears + " in dog years.");
+
