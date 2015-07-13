54c4be5c17512b82bac04015a47b1a7a21ee435b
NBESS <nicholasdbess@gmail.com>
2015-07-13 09:56:29 -0400

    Updates missing folders

--- VariablePractice/js/script.js
+++ VariablePractice/js/script.js
@@ -17,9 +17,19 @@ var mothersAge = 55;  // number variable
 
 var isStudent = false;
 
+
 var firstName = "Nick";
+var lastName = "Bess";
+var fullName = firstName + " " + lastName;
 
 var muppetNames = ["Kermit", "Miss Piggy" , firstName , "Gonzo"]; //0,1,2,3
 
 console.log(muppetNames[3]);
 
+console.log(fullName);
+
+var a = 6;
+var b = 7;
+
+var result = a + b;
+console.log(result);
