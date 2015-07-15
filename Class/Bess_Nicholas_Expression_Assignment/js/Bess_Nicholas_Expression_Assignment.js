88c5bd8fa0ee2cd63ca2a4f1f5ebe8817bdaa3fb
NBESS <nicholasdbess@gmail.com>
2015-07-15 19:07:19 -0400

    Initial comments for Expression Assignment

---
+++ Class/Bess_Nicholas_Expression_Assignment/js/Bess_Nicholas_Expression_Assignment.js
@@ -0,0 +1,21 @@
+/*
+ * Nicholas Bess
+ * 15 July 2015
+ * Expression Assignment
+ */
+
+
+//alert("JavaScript works fine."); //... ensures that js file is linked to the HTML page.
+
+
+// Enter Variables Here
+
+
+
+
+//Enter Alerts Here
+
+
+
+
+// Enter Final Result Here
