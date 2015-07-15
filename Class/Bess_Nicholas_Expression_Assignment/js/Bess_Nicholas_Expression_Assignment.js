f1525fed31f3cfa606abb238912998b634af0ed5
NBESS <nicholasdbess@gmail.com>
2015-07-15 19:22:21 -0400

    Adds overview of the process (comment) and 3 variables

--- Class/Bess_Nicholas_Expression_Assignment/js/Bess_Nicholas_Expression_Assignment.js
+++ Class/Bess_Nicholas_Expression_Assignment/js/Bess_Nicholas_Expression_Assignment.js
@@ -5,10 +5,15 @@
  */
 
 
-//alert("JavaScript works fine."); //... ensures that js file is linked to the HTML page.
+// alert("JavaScript works fine."); //... ensures that js file is linked to the HTML page.
 
+/* The object of this code is to calculate the amount an individual will have remaining in her/his wallet after
+ * purchasing up to 3 items at a discounted rate (the code will still yield the correct amount if the discount is 0%.
+ */
 
-// Enter Variables Here
+var walletAmount           = prompt("Enter your spending budget.");             // asks the user to input the total amount of cash on-hand for purchase
+var discountPercent        = prompt("Enter the percent of the discount.");      // receives the percent of the discount from the user
+discountPercent           /= 100;                                               // divides the percent by 100 to convert into a floating-point number
 
 
 
