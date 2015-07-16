f851c3f7944c64122b5b67b4fa52b6975d6cc8d2
NBESS <nicholasdbess@gmail.com>
2015-07-15 20:05:59 -0400

    Adds alerts and updates some previous comments

--- Class/Bess_Nicholas_Expression_Assignment/js/Bess_Nicholas_Expression_Assignment.js
+++ Class/Bess_Nicholas_Expression_Assignment/js/Bess_Nicholas_Expression_Assignment.js
@@ -15,14 +15,18 @@ var walletAmount           = prompt("Enter your spending budget.");  // asks the
 var discountPercent        = prompt("Enter the percent of the discount.");  // receives the percent of the discount from the user
 discountPercent           /= 100;  // divides the percent by 100 to convert into a floating-point number (i.e.. 10 / 100 = .1)
 var originalAmount         = [prompt("Enter the cost of the first item."), prompt("Enter the cost of the second item.\nNote: If you do not have a second item enter 0."), prompt("Enter the cost of the third item.\nNote: If you do not have a third item enter 0.")];  // the user inputs the cost of each item into the browser. Minimum number of items entered = 1 ; max = 3
-//console.log(originalAmount); //... confirms that the numbers input by user are printing to console
-var discount               = (Number(originalAmount[0]) + Number(originalAmount[1]) + Number(originalAmount[2])) * discountPercent;  //multiplies the total cost of all items by the discountPercent (i.e.. 50 * .10)
-//console.log(discount); //confirms that the discount string is converted to a number and accurately calculated
-var discountedPrice        = Number(originalAmount[0]) + Number(originalAmount[1]) + Number(originalAmount[2]) - discount;  //calculates the amount of the purchase by subtracting the amount of the discount from the original price of all items, before the discount was applied.
-//console.log(discountedPrice); //confirms that the discount string is converted to a number and accurately calculated
+//console.log(originalAmount); // confirms that the numbers input by user are printing to console
+var discount               = (Number(originalAmount[0]) + Number(originalAmount[1]) + Number(originalAmount[2])) * discountPercent;  // multiplies the total cost of all items by the discountPercent (i.e.. 50 * .10)
+//console.log(discount); // confirms that the discount string is converted to a number and accurately calculated, by printing to the console
+var discountedPrice        = Number(originalAmount[0]) + Number(originalAmount[1]) + Number(originalAmount[2]) - discount;  // calculates the amount of the purchase by subtracting the amount of the discount from the original price of all items, before the discount was applied.
+//console.log(discountedPrice); // confirms that the discount string is converted to a number and accurately calculated, by printing to the console
 
 
-//Enter Alerts Here
+alert("You currently have $" + walletAmount + " in your wallet.");  // displays the initial balance of the users wallet in the browser
+console.log("You currently have $" + walletAmount + " in your wallet.");  // prints the initial balance of the users wallet to the console
+
+alert("You will spend $" + discountedPrice + ".");  // displays the amount that will be subtracted from the users wallet in the browser
+console.log("You will spend $" + discountedPrice + ".");  // prints the amount that will be subtracted from the users wallet to the console
 
 
 
