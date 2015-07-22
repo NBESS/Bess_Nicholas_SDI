af8f61e277b654149e65031e6003731873885c60
NBESS <nicholasdbess@gmail.com>
2015-07-22 15:34:19 -0400

    Tire Pressure example

--- Class/ActivityFiles/Conditionals_worksheet/js/script.js
+++ Class/ActivityFiles/Conditionals_worksheet/js/script.js
@@ -1,4 +1,4 @@
-// Does contestant weigh enough to enter heavyweight division?
+/*// Does contestant weigh enough to enter heavyweight division?
 var weight = Number(prompt("Enter your weight."));
 //If the competitor is less than 215 he needs to gain some weight
 if(weight < 215){
@@ -8,3 +8,20 @@ if(weight < 215){
     //if its greater than or equal to 215
     console.log("You qualify for the heavy weight division!")
 }
+*/
+
+/*
+//Tire Pressure
+//If two front tires have equal pressure and the two rear tires have equal pressure, the car meets standards
+//what is the PSI of each tire?
+var tirePressure = [(Number(prompt("Enter the pressure of the front tire on the driver's side."))), (Number(prompt("Enter the pressure of the front tire on the passenger's side."))), (Number(prompt("Enter the pressure of the rear tire on the driver's side."))), (Number(prompt("Enter the pressure of the rear tire on the passenger's side.")))];
+//if the front tires have the same pressure and the rear tires have the same pressure
+if(tirePressure[0] === tirePressure[1] && tirePressure[2] === tirePressure[3]){
+    //the tires pass spec
+    console.log("The tires pass spec!")
+}else{
+    //the tires need to be checked
+    console.log("Get your tires checked out!!!")
+}
+*/
+
