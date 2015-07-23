dcf9f7ead5a05245f1ae80fd79026263097cc17b
NBESS <nicholasdbess@gmail.com>
2015-07-23 10:03:09 -0400

    Conditionals Assignment initial commit with comments

---
+++ Class/Bess_Nicholas_Conditionals_Assignment/js/Bess_Nicholas_Conditionals_Assignment.js
@@ -0,0 +1,29 @@
+/*Nicholas Bess
+  July 24, 2015
+  Conditionals Assignment
+ */
+
+
+
+
+
+//Enter Variables
+
+
+
+
+
+//Enter Prompts (Ensure that all prompts are validated)
+
+
+
+
+
+//Enter Conditional Statement with at least one Logical Operator (&&, || or !)
+
+
+
+
+
+//Enter Ternary Conditional Statement
+
