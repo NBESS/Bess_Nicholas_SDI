632ecf3dba007db20640dc95cba487207cde3e43
NBESS <nicholasdbess@gmail.com>
2015-07-29 11:43:10 -0400

    First commit with initial comments

---
+++ Class/Bess_Nicholas_Functions_Assignment/js/Bess_Nicholas_Fucntions_Assignment.js
@@ -0,0 +1,28 @@
+/**
+ * Nicholas Bess
+ * July 29, 2015
+ * Functions Assignment
+ */
+
+//alert('JavaScript is working!');
+
+
+//variables
+
+
+
+
+
+//functions
+
+
+
+
+
+//main code
+
+
+
+
+
+//test values
